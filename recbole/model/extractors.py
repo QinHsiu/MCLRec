c0e9e788aac459c4fb0dd5b194d92aa4c10f1594
QinHsiu <2278552853@qq.com>
2024-01-05 10:38:45 +0800

    add att&cnn augmentor

--- recbole/model/extractors.py
+++ recbole/model/extractors.py
@@ -53,3 +53,58 @@ class Extractor(nn.Module):
         output=self.dense_3(output)
         output=self.normal(output)
         return output
+
+
+# Attention-Augmenter
+class Att_Extractor(nn.Module):
+    def __init__(self,layers, activation='gelu', init_method=None):
+        super(Att_Extractor, self).__init__()
+        # q,k layers
+        self.dense_1=nn.Linear(layers[0],layers[1])
+        self.dense_2=nn.Linear(layers[0],layers[1])
+        # v layer
+        self.dense_3=nn.Linear(layers[0],layers[1])
+        if activation!=None:
+            self.actfunction = ACT2FN[activation]
+        self.normal=Normalize()
+
+    def forward(self,input): # [B H]
+        q_output=self.dense_1(input)
+        k_output=self.dense_2(input)
+        v_output=self.dense_3(input)
+        attention_scores = torch.matmul(q_output, k_output.transpose(-1, -2))
+        attention_probs = nn.Softmax(dim=-1)(attention_scores)
+        output = torch.matmul(attention_probs, v_output) # [B H]
+        if self.actfunction!=None:
+            output=self.actfunction(output)
+        output=self.normal(output)
+        return output
+
+
+# CNN-Augmenter
+class CNN_Extractor(nn.Module):
+    def __init__(self,layers, activation='gelu', init_method=None,kernel_size_list=[3,4,5],stride=1):
+        super(CNN_Extractor, self).__init__()
+        self.dense_1=nn.Linear(layers[0],layers[1]-1+kernel_size_list[0])
+        self.dense_2=nn.Linear(layers[0],layers[1]-1+kernel_size_list[1])
+        self.dense_3=nn.Linear(layers[0],layers[1]-1+kernel_size_list[2])
+        # B*B
+        self.conv_1=nn.Conv1d(layers[0],layers[1],kernel_size_list[0],stride=stride)
+        self.conv_2=nn.Conv1d(layers[0],layers[1],kernel_size_list[1],stride=stride)
+        self.conv_3=nn.Conv1d(layers[0],layers[1],kernel_size_list[2],stride=stride)
+        if activation!=None:
+            self.actfunction = ACT2FN[activation]
+        self.normal=Normalize()
+    def forward(self,input): # [B H]
+        # layer 1
+        output1=self.conv_1(self.dense_1(input).unsqueeze(0)).squeeze(0)
+        # layer 2
+        output2=self.conv_2(self.dense_2(input).unsqueeze(0)).squeeze(0)
+        # layer 3
+        output3=self.conv_3(self.dense_3(input).unsqueeze(0)).squeeze(0)
+        output=output1+output2+output3
+        if self.actfunction!=None:
+            output=self.actfunction(output)
+        output=self.normal(output)
+        return output
+
